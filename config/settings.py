b41bdf210002da9da20af8455a989f953cbe0ca2
aliasttt <aliasadi3853@gmail.com>
2026-02-18 15:53:24 +0330

    توضیحات تغییرات

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dl=j@qxjvlo&$%7a@d1$i84&scr3ghs35i#p1@nj%8*y(u+i8e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
